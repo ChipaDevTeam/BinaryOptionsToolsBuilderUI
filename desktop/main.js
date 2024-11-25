7e91733b49a1005c4e4774c345aa449b701c5bbd
vigo <vigopaul05@gmail.com>
2024-11-25 17:01:43 -0300

    Basic

---
+++ desktop/main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+    },
+  });
+
+  mainWindow.loadURL('http://localhost:3000');
+  mainWindow.on('closed', () => (mainWindow = null));
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) app.emit('ready');
+});
